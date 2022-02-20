2764a044acae591f5186c6e29b940ca0604d1cd1
MergenStudios <paulthorand.2005@gmail.com>
2022-02-20 15:59:39 +1030

    changed variables to match go naming conventions

--- scripts/import.go
+++ scripts/import.go
@@ -20,68 +20,68 @@ func ImportDatapoints(dataType string, inputPath string) {
 	}
 
 	// load the data type enums here
-	type_enums, err := utils.LoadConfig()
+	typeEnums, err := utils.LoadConfig()
 	if utils.Handle(err) != nil { return }
-	data_points := make(map[string][]structs.DataPoint)
+	dataPoints := make(map[string][]structs.DataPoint)
 
 	if _, err := os.Stat(inputPath); err == nil {
 		// walk through every file in the path
-		err = filepath.Walk(inputPath, func(file_path string, info os.FileInfo, err error) error {
+		err = filepath.Walk(inputPath, func(filePath string, info os.FileInfo, err error) error {
 			fmt.Println(info.Name())
 
 			// for every file extensions the give format can possibly have
-			for _, extension := range type_enums[dataType].Extensions {
+			for _, extension := range typeEnums[dataType].Extensions {
 				extension = "." + extension
 				// if the extension of the file matches one of the possible extensions
-				if extension == filepath.Ext(file_path) {
+				if extension == filepath.Ext(filePath) {
 					// get the creation time of the file
-					var start_time time.Time = time.Time{}
-					var end_time time.Time = time.Time{}
+					var startTime time.Time = time.Time{}
+					var endTime time.Time = time.Time{}
 
-					if type_enums[dataType].DetermineTime == "mtime" {
-						start_time = info.ModTime().In(timezone)
-					} else if type_enums[dataType].DetermineTime == "video" {
+					if typeEnums[dataType].DetermineTime == "mtime" {
+						startTime = info.ModTime().In(timezone)
+					} else if typeEnums[dataType].DetermineTime == "video" {
 
-						io_reader, err := os.Open(file_path)
+						ioReader, err := os.Open(filePath)
 						if err != nil {
 							return err
 						}
 
-						defer io_reader.Close()
-						file_duration, err := utils.GetMP4Duration(io_reader)
+						defer ioReader.Close()
+						fileDuration, err := utils.GetMP4Duration(ioReader)
 						if err != nil { return err}
 
 						// calculate the start_time_parsed
-						start_time = info.ModTime().Add(time.Duration(-(int64(file_duration))) * time.Second)
-						end_time = info.ModTime()
+						startTime = info.ModTime().Add(time.Duration(-(int64(fileDuration))) * time.Second)
+						endTime = info.ModTime()
 					}
 
 
 					// creat the datapoint
-					var data_point structs.DataPoint
-					if type_enums[dataType].Dated == "point" {
-						data_point = structs.DataPoint{
-							type_enums[dataType].Dated,
-							start_time,
+					var dataPoint structs.DataPoint
+					if typeEnums[dataType].Dated == "point" {
+						dataPoint = structs.DataPoint{
+							typeEnums[dataType].Dated,
+							startTime,
 							time.Time{},
 							dataType,
-							file_path,
+							filePath,
 						}
-					} else if type_enums[dataType].Dated == "range" {
-						data_point = structs.DataPoint{
-							type_enums[dataType].Dated,
-							start_time,
-							end_time,
+					} else if typeEnums[dataType].Dated == "range" {
+						dataPoint = structs.DataPoint{
+							typeEnums[dataType].Dated,
+							startTime,
+							endTime,
 							dataType,
-							file_path,
+							filePath,
 						}
 					}
 
 					// if the key doesnt exist, creat it and make the value an empty list of data points
-					if _, ok := data_points[start_time.Format("2006-01")]; !ok {
-						data_points[start_time.Format("2006-01")] = []structs.DataPoint{}
+					if _, ok := dataPoints[startTime.Format("2006-01")]; !ok {
+						dataPoints[startTime.Format("2006-01")] = []structs.DataPoint{}
 					}
-					data_points[start_time.Format("2006-01")] = append(data_points[start_time.Format("2006-01")], data_point)
+					dataPoints[startTime.Format("2006-01")] = append(dataPoints[startTime.Format("2006-01")], dataPoint)
 				}
 			}
 			return err
@@ -91,27 +91,27 @@ func ImportDatapoints(dataType string, inputPath string) {
 		}
 	}
 
-	for key, da_data_points := range data_points {
+	for key, daDataPoints := range dataPoints {
 
-		var month_data structs.MonthData
-		gob_path := "./data/" + key + ".gob"
+		var monthData structs.MonthData
+		gobPath := "./data/" + key + ".gob"
 
 		// if the gob file exists
-		if _, err := os.Stat(gob_path); err == nil {
-			err = utils.ReadGob(gob_path, &month_data)
+		if _, err := os.Stat(gobPath); err == nil {
+			err = utils.ReadGob(gobPath, &monthData)
 			if utils.Handle(err) != nil { return }
 
-			month_data.Data = append(month_data.Data, da_data_points...)
+			monthData.Data = append(monthData.Data, daDataPoints...)
 		} else {
-			split_string := strings.Split(key, "-")
+			splitString := strings.Split(key, "-")
 
-			year, err := strconv.ParseInt(split_string[0], 10, 64)
+			year, err := strconv.ParseInt(splitString[0], 10, 64)
 			if utils.Handle(err) != nil { return }
 
-			month, err := strconv.ParseInt(split_string[1], 10, 64)
+			month, err := strconv.ParseInt(splitString[1], 10, 64)
 			if utils.Handle(err) != nil { return }
 
-			month_data.StartTime = time.Date(
+			monthData.StartTime = time.Date(
 				int(year),
 				time.Month(month),
 				00,
@@ -122,18 +122,18 @@ func ImportDatapoints(dataType string, inputPath string) {
 				timezone,
 			)
 
-			month_data.EndTime = month_data.StartTime.AddDate(0, 1, 0)
+			monthData.EndTime = monthData.StartTime.AddDate(0, 1, 0)
 
-			month_data.Data = da_data_points
+			monthData.Data = daDataPoints
 		}
 
 		// sort the array of unsorted data points
-		sort.Slice(month_data.Data[:], func(i, j int) bool {
-			return month_data.Data[i].Start.Before(month_data.Data[j].Start)
+		sort.Slice(monthData.Data[:], func(i, j int) bool {
+			return monthData.Data[i].Start.Before(monthData.Data[j].Start)
 		})
 
 		// serialize the now sorted month data
-		err := utils.WriteGob(gob_path, month_data)
+		err := utils.WriteGob(gobPath, monthData)
 		if utils.Handle(err) != nil { return }
 
 	}
